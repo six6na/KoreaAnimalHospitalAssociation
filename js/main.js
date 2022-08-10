a8fa8d2f6242a63bf41d9b03dcea40a77cef38d8
six6na <105402240+six6na@users.noreply.github.com>
2022-08-11 01:25:33 +0900

    main-noti/news

---
+++ js/main.js
@@ -0,0 +1,114 @@
+// main-header nav
+
+$(function(){
+    $("ul.gnb>li").hover(function(){
+        $("ul.sub, .nav-bg").stop().slideDown();
+    }, function(){
+        $("ul.sub, .nav-bg").stop().slideUp();
+    });
+});
+
+// main-visual slide
+
+$(function(){
+    const swiper = new Swiper('.swiper', {
+        direction: 'horizontal',
+        loop: true,
+        autoplay: true,
+        speed : 1000,
+        
+        pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        },
+        
+        autoplay: {
+          delay:2000,
+          disableOnInteraction: false
+        },
+      
+      });
+});
+
+// count
+
+$(function() {
+    var cnt0 = 0;
+  
+    counterFn();
+  
+    function counterFn() {
+  
+      id0 = setInterval(count0Fn, 500);
+  
+      function count0Fn() {
+        cnt0++;
+        if (cnt0 > 11 ) {
+          clearInterval(id0);
+        } else {
+          $(".number1").text(cnt0);
+        }
+      }
+    }
+  });
+  
+  $(function() {
+    var cnt0 = 0;
+  
+    counterFn();
+  
+    function counterFn() {
+  
+      id0 = setInterval(count0Fn, 60);
+  
+      function count0Fn() {
+        cnt0++;
+        if (cnt0 > 91 ) {
+          clearInterval(id0);
+        } else {
+          $(".number2").text(cnt0);
+        }
+      }
+    }
+  });
+  
+  $(function() {
+    var cnt0 = 0;
+  
+    counterFn();
+  
+    function counterFn() {
+  
+      id0 = setInterval(count0Fn, 1);
+  
+      function count0Fn() {
+        cnt0++;
+        if (cnt0 > 1200 ) {
+          clearInterval(id0);
+        } else {
+          $(".number3").text(cnt0);
+        }
+      }
+    }
+  });
+  
+  // tab btn
+  
+  $(function(){
+    $('#news').addClass('on');
+  
+    $('.news-btn').click(function(){
+      $('#news').addClass('on');
+      $('#notice').removeClass('on');
+      $(this).css({'background':'#155299','color':'#fff'});
+      $('.notice-btn').css({'background':'#fff', 'color':'#000'});
+    });
+  
+    
+    $('.notice-btn').click(function(){
+      $('#notice').addClass('on');
+      $('#news').removeClass('on');
+      $(this).css({'background':'#155299','color':'#fff'});
+      $('.news-btn').css({'background':'#fff', 'color':'#000'});
+    });
+  });
